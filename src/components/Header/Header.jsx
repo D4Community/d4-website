7191ed3ad6c9fb32527df7421987ec423cc9650a
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-01-13 03:29:50 +0530

    commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import people from '../../assets/people.png';
+import Spline from "@splinetool/react-spline";
+// import globe from '../../assets/globe-500.h264.gif';
+import "./Header.css";
+const Header = () => (
+  <div className="d4__header section__padding" id="home">
+    <div className="d4__header-content">
+      <h1 className="gradient__text">
+        Let&apos;s Build Something amazing with
+      </h1>
+      <span className="gradient__text"> D4-Community</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ab cum
+        asperiores consequatur corporis reprehenderit, accusantium laudantium
+        laboriosam saepe explicabo ullam velit quasi iure repudiandae!
+      </p>
+    </div>
+
+    <div className="d4__header-image">
+      {" "}
+      <Spline scene="https://prod.spline.design/lEwM4nvkbtM9sUdJ/scene.splinecode" />
+    </div>
+  </div>
+);
+
+export default Header;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="d4_navbar">
+      <div className="d4_navbar_links">
+        <div className="d4_navbar_links_logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="d4_navbar_links_container">
+          <p>
+            <a href="#Home">Home</a>
+          </p>
+          <p>
+            <a href="#WhatisD4">What is D4</a>
+          </p>
+          <p>
+            <a href="#Team">Team</a>
+          </p>
+          <p>
+            <a href="#about us">About us</a>
+          </p>
+        </div>
+      </div>
+      <div className="d4_navbar_joinus">
+        <button type="button">Join us</button>
+      </div>
+      <div className="d4_navbar_menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+
+        {toggleMenu && (
+          <div className="d4_navbar_menu_container scale_up_center">
+            <div className="d4_navbar_menu_container_links">
+              {" "}
+              <p>
+                <a href="#Home">Home</a>
+              </p>
+              <p>
+                <a href="#WhatisD4">What is D4</a>
+              </p>
+              <p>
+                <a href="#Team">Team</a>
+              </p>
+              <p>
+              <a href="#Events">Events</a>
+              </p>
+              <p>
+                <a href="#about us">About us</a>
+              </p>
+            </div>
+            <div className="d4_navbar_menu_container_links_joinus">
+              <button type="button">Join us</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
