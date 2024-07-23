3023847b9bef68d580e48bd0f696a3d5c84f878b
AarinGuleria <aaringuleria2310@gmail.com>
2024-07-23 15:57:29 +0530

    Scrolling-GotoBtn

--- src/App.js
+++ src/App.js
@@ -6,10 +6,25 @@ import Navbar from "./components/Navbar/Navbar";
 import WhatisD4 from "./components/WhatisD4/WhatisD4";
 import UpcomingEvents from "./components/Upcoming Events/UpcomingEvents";
 import Footer from "./components/Footer/Footer";
+import GoToTop from "./components/GoToTop/GoToTop"; 
+import { ThemeProvider } from 'styled-components';
 
+const theme = {
+  btn: {
+    backgroundColor: '#000',
+  },
+  colors: {
+    shadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+  },
+  media: {
+    mobile: '768px',
+  },
+};
 
-const App = () => {
+function App() {
   return (
+    <ThemeProvider theme={theme}>
+      return (
     <div className="App">
       <div className="gradient__bg">
         <Navbar />
@@ -17,10 +32,30 @@ const App = () => {
         <WhatisD4 />
         <Members/> 
         <UpcomingEvents/>
+        <GoToTop />
         <Footer/>
       </div>
     </div>
   );
-};
+    </ThemeProvider>
+  );
+}
 
 export default App;
+
+// const App = () => {
+
+//   return (
+//     <div className="App">
+//       <div className="gradient__bg">
+//         <Navbar />
+//         <Header />
+//         <WhatisD4 />
+//         <Members/> 
+//         <UpcomingEvents/>
+//         <GoToTop />
+//         <Footer/>
+//       </div>
+//     </div>
+//   );
+// };
